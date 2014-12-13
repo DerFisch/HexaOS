87bbe06383240f0b3c52f4b2767e6e9623a45ff9
lewislovesgames <lewisminecraft@hotmail.co.uk>
2014-12-13 15:00:04 +0000

    Create login.lua

---
+++ src/hexa/login.lua
@@ -0,0 +1,19 @@
+local image = paintutils.loadImage("hexa/gui/login")
+paintutils.drawImage(image, 1, 1)
+term.setCursorPos(1,13)
+write("Please enter your username: ")
+local name = read()
+file = fs.open("/disk/data/.name","r")
+file.readLine()
+--file.close() --the file handle will coklose before it can read it.othxka y
+if name == file.readLine() then -- i'll fix
+write("Password: ")
+local pass = read("*")
+file2 = fs.open("/disk/data/.pass","r")
+file.readLine()
+if pass == file.readLine() then
+print("Correct!")
+end
+end
+file.close() -- put everything u want to do with ".name" above here.
+file2.close()
